ac89cbd4e816d0ed72ac4e4770ee146c194779a1
11249a015-netizen <11249a015@kanchiuniv.ac.in>
2026-08-05 10:45:16 +0530

    Add files via upload

---
+++ StaticDemo1.java
@@ -0,0 +1,9 @@
+class StaticDemo1 {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum = " + add(10, 20));
+    }
+}
