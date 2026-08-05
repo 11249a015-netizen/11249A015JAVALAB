de59aeaaca545c9aefcb42957289b77e7e5e179b
11249a015-netizen <11249a015@kanchiuniv.ac.in>
2026-08-05 10:45:35 +0530

    Add files via upload

---
+++ StaticDemo2.java
@@ -0,0 +1,9 @@
+class StaticDemo2 {
+    static int square(int n) {
+        return n * n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Square = " + square(7));
+    }
+}
