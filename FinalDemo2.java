9e2ff677e9523120665a6e75e95110771fc9e784
11249a015-netizen <11249a015@kanchiuniv.ac.in>
2026-08-05 10:46:19 +0530

    Add files via upload

---
+++ FinalDemo2.java
@@ -0,0 +1,10 @@
+class FinalDemo2 {
+    final void display() {
+        System.out.println("This is a final method");
+    }
+
+    public static void main(String[] args) {
+        FinalDemo2 obj = new FinalDemo2();
+        obj.display();
+    }
+}
