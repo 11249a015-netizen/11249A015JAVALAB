424f9ad1266b155df6cd20e0308f9bbbe9022cd0
11249a015-netizen <11249a015@kanchiuniv.ac.in>
2026-08-12 11:37:27 +0530

    Add files via upload

---
+++ ExceptionHandling.java
@@ -0,0 +1,12 @@
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        try {
+            int a = 10, b = 0;
+            int c = a / b;
+            System.out.println("Result = " + c);
+        } 
+        catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
